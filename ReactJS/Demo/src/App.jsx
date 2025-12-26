3b6e109edb52b1af16d47801b397542e3602950e
RakshaPandeyy <raksha.pandeyyy@gmail.com>
2025-12-26 18:54:29 +0530

    setting up React

---
+++ ReactJS/Demo/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 id="head">Hello World</h1>
+      <div>this is my first react app</div>
+    </>
+  );
+}
+
+export default App;

53c29cffb16cd8f8f034c273ed850325f31df28d
RakshaPandeyy <raksha.pandeyyy@gmail.com>
2026-01-11 19:18:21 +0530

    frontend backend connectivity

---
+++ Cravingz/client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+const App = () => {
+
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
